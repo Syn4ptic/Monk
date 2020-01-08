d425b7891bbd507a7df208ed726c308f2c854bc9
syn4ptic <syn4ptic@gmail.com>
2020-01-08 15:40:30 +0100

    path added

--- monk.py
+++ monk.py
@@ -200,6 +200,7 @@ if __name__ == "__main__":
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     #path = r"C:\Users\marcin.kawka\Downloads"
+    #path = r"/storage/emulated/0/Download/"
     path = r"C:\Users\marcin.kawka\OneDrive - DSV\Desktop"
     print(path)
     
